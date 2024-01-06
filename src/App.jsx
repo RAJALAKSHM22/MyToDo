b9f65ab56ab8b27db0980f9e7261717bb146a878
RAJALAKSHM22 <rajalakshmiasokan500@gmail.com>
2024-01-06 18:03:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Title from './component/Title'
+import Sidebar from './component/sidebar'
+import Dashboard from './component/dashboard'
+
+function App() {
+  return <>
+    <div id="wrapper">
+    <Title/>
+
+    <Sidebar/>
+    <Dashboard/>
+
+    </div>
+
+   
+
+  </>
+}
+
+export default App
