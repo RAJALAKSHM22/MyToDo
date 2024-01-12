644f3a40930ed1cc37e44c403d0861bdc4c7fc35
RAJALAKSHM22 <rajalakshmiasokan500@gmail.com>
2024-01-12 14:50:33 +0530

    comit it

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
 
     <BrowserRouter>
         <div id="wrapper">
-        <Sidebar/>
+        {/* <Sidebar/> */}
 
         {/* <Routes> */}
         <div id="content-wrapper" className="d-flex flex-column">
@@ -80,61 +80,20 @@ function App() {
                   <h1 className="h3 mb-0 text-gray-800"><i><b>Dashboard</b></i></h1>
                 </div>
                   {/* Page Slot */}
-                <div className="row"> 
+                {/* <div className="row"> 
                   {
                     slots.map((e,i)=>{
                       return <Dashboard slots={e} key={i} />
                     })
                   }
-                </div>
+                </div> */}
                   {/* User List */}
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
                   <h1 className="h3 mb-0 text-gray-800"><i>User List</i></h1>
                 </div>  
                   {/* User Table  */}
                   <Page user={user} setuser={setuser}/>
-                {/* <div className="row">
-
-                  <div className="container-fluid">
-                    <Table striped bordered hover>
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>Name</th>
-                          <th>Id</th>
-                          <th>eMail</th>
-                          <th>Mobile</th>
-                          <th>Describe</th>                      
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {
-                          user.map((e,i)=>{
-                            return <tr key={i}>
-                              <td>{i+1}</td>
-                              <td>{e.Name}</td>
-                              <td>{e.Id}</td>
-                              <td>{e.eMail}</td>
-                              <td>{e.Mobile}</td>
-                              <td>{e.Describe}</td>
-                              <td>
-                                <Button variant="light"> EDIT </Button>
-                                &nbsp;
-                                &nbsp; 
-                                <Button variant="danger" onClick={()=>{
-                                  handledelet(i)
-                                }}> DELET </Button>
-
-                              </td>
-
-                            </tr>
-                          })
-                        }
-                      </tbody>
-                    </Table> 
-                    </div>
-
-                </div>   */}
+                
             </div> 
           </div>
         </div>  
